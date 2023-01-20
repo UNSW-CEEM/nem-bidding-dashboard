ccb230ca64aa21f6c67eab2fa8f37725a31dc0cc
nick-gorman <40549624+nick-gorman@users.noreply.github.com>
2023-01-20 11:43:43 +1100

    update version number

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -23,7 +23,7 @@ copyright = "2022, Nicholas Gorman and Patrick Chambers"
 author = "Nicholas Gorman and Patrick Chambers"
 
 # The full version, including alpha/beta/rc tags
-release = "0.7.0"
+release = "0.10.0"
 
 
 # -- General configuration ---------------------------------------------------
--- src/nem_bidding_dashboard/layout_template.py
+++ src/nem_bidding_dashboard/layout_template.py
@@ -107,7 +107,7 @@ def build_banner():
                     ),
                     html.A(
                         "Access data",
-                        href="https://nem-bidding-dashboard.readthedocs.io/en/latest/examples.html#getting-the-data-behind-the-web-app-visualisation",
+                        href="https://nem-bidding-dashboard.readthedocs.io/en/latest/examples.html#getting-the-data-behind-the-web-app-visualisations",
                         target="_blank",
                         style={"color": "black", "margin-left": "10px"},
                     ),
