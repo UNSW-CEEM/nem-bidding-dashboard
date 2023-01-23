66bd5ff810d0e28bc70894416ca5912a3f0df539
nick-gorman <40549624+nick-gorman@users.noreply.github.com>
2023-01-23 15:08:02 +1100

    update verion number

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -23,7 +23,7 @@ copyright = "2022, Nicholas Gorman and Patrick Chambers"
 author = "Nicholas Gorman and Patrick Chambers"
 
 # The full version, including alpha/beta/rc tags
-release = "1.0.0"
+release = "1.0.1"
 
 
 # -- General configuration ---------------------------------------------------
